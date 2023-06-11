7985c8e43cf76cdb20eff8b6f7b183f10bc86373
Dmitrij <fazzy07@tut.by>
2023-06-11 23:09:12 +0300

    поиск простых чисел

---
+++ SimpleNumbers.java
@@ -0,0 +1,18 @@
+public class SimpleNumbers {
+    public static void main(String [] args) {
+        for (int i = 2; i <= 1000; i++) {
+            boolean isPrime = true;
+            for (int j = 2; j < i; j++) {
+                if ( i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+
+            }
+            if(isPrime) {
+                System.out.println(i);
+            }
+        }
+
+        }
+    }
