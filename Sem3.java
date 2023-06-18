b5d3e9f04737a5db7e624ef5fc73cd339bdfb818
Dmitrij <fazzy07@tut.by>
2023-06-18 10:15:08 +0300

    пирамидальная сортировка

---
+++ Sem3.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Iterator;
+
+public class Sem3 {
+    public static void main(String[] args) {
+    //    Создать список типа ArrayList. Поместить в него как строки, так и целые числа. 
+    // Пройти по списку, найти и удалить целые числа.
+        List<Object> array = new ArrayList<>(Arrays.asList("Земля",12, "Земля", 1,"Земля", 3, 4, 4.2, "Земля", "Земля"));
+
+        // for (int i = 0; i < array.size(); i++) {
+        //     if (array.get(i) instanceof Integer){
+        //         array.remove(i);
+        //         i--;
+        //     }
+        // }
+        
+
+        // for (int i = array.size()-1; i >= 0; i--) {
+        //     if (array.get(i) instanceof Integer){
+        //         array.remove(i);
+        // }
+        // }
+
+        Iterator iter = array.iterator();
+        while (iter.hasNext()){
+            if (iter.next() instanceof Integer){
+                iter.remove();
+            }
+        }
+        System.out.println(array);
+    }
+}
