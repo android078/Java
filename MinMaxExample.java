bd36d94e69395b4bc8f56dcc8ebfb1a8d8aebff9
Dmitrij <fazzy07@tut.by>
2023-06-19 23:07:32 +0300

    максимальное/минимальное число и среднее арифметическое

--- MinMaxExample.java
+++ MinMaxExample.java
@@ -1,28 +1,39 @@
 import java.util.ArrayList;
+import java.util.Random;
+import static java.lang.Math.round;
+import static java.util.Collections.max;
+import static java.util.Collections.min;
+
 
 public class MinMaxExample {
     public static void main(String[] args) {
-       ArrayList<Integer> list = new ArrayList<>();
-
-list.add(10);
-list.add(2);
-list.add(6);
-list.add(25);
-list.add(72);
-list.add(48);
-
-
-
-
-int min = (int) list.get(0);
-int max = (int) list.get(0);
-
-for (Integer i: list) {
-    if(i < min) min = i; if(i > max) 
-        max = i;
-}
-
-System.out.println("Минимальное число: " + min);
-System.out.println("Максимальное число: " + max);
+       Random rnd = new Random();
+        ArrayList<Integer> list1 = new ArrayList<Integer>();
+        for (int i = 0; i < 10; i++) {
+            int val = rnd.nextInt(1, 100);
+            list1.add(val);
+        }
+        System.out.printf("Первоначальный список %s\n", list1);
+        int max = max(list1);
+        int min = min(list1);
+        int maxItem = list1.get(0);
+        int minItem = list1.get(0);
+        int sumItems = 0;
+        for (int item : list1) {
+            if (item > maxItem) {
+                maxItem = item;
+            }
+            if (item < minItem) {
+                minItem = item;
+            }
+            sumItems += item;
+        }
+        float average = (float)sumItems/list1.size();
+        System.out.printf("Максимальный элемент при помощи max %s\n", max);
+        System.out.printf("Максимальный элемент %s\n", maxItem);
+        System.out.printf("Минимальный элемент при помощи min %s\n", min);
+        System.out.printf("Минимальный элемент %s\n", minItem);
+        System.out.printf("Сумма элементов %s\n", sumItems);
+        System.out.printf("Среднее арифметическое %s\n", average);
     }
 }
